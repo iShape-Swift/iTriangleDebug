e2793379218d94a84a67fa041ee8a3dbeb4bb84f
Nail Sharipov <nailxsharipov@gmail.com>
2023-07-10 16:34:03 +0300

    empty

---
+++ iTriangleDebug/Main/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  iTriangleDebug
+//
+//  Created by Nail Sharipov on 10.07.2023.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ iTriangleDebug/iTriangleDebugApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTriangleDebugApp.swift
+//  iTriangleDebug
+//
+//  Created by Nail Sharipov on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct iTriangleDebugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
