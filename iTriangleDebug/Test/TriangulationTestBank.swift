de3fc7dd1117f983c5e83fe5dcb876b08b705255
Nail Sharipov <nailxsharipov@gmail.com>
2023-11-26 20:40:28 +0300

    copy tests

---
+++ iTriangleDebug/Test/TriangulationTestBank.swift
@@ -0,0 +1,121 @@
+//
+//  TriangulationTestBank.swift
+//  iTriangleDebug
+//
+//  Created by Nail Sharipov on 25.11.2023.
+//
+
+
+import iShape
+import iFixFloat
+import Foundation
+import simd
+import iTriangle
+
+struct TriangulationTest: Decodable {
+    let scale: Int64
+    let shape: FixShape
+    let points: FixPath
+    let indices: [Int]
+    let polygons: [ConvexPath]
+
+    enum CodingKeys: String, CodingKey {
+        case scale
+        case shape
+        case points
+        case indices
+        case polygons
+    }
+}
+
+extension ConvexPath: Decodable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let path = try container.decode(FixPath.self, forKey: .path)
+        let side = try container.decode([ConvexSide].self, forKey: .side)
+        self.init(path: path, side: side)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case path
+        case side
+    }
+}
+
+extension FixShape: Decodable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let paths = try container.decode([FixPath].self, forKey: .paths)
+        self.init(paths: paths)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case paths
+    }
+}
+
+extension ConvexSide: Decodable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let value = try container.decode(UInt8.self)
+        self = ConvexSide(rawValue: value) ?? .outer
+    }
+}
+struct TriangulationTestBank {
+
+    private var folderUrl: URL {
+        guard let folderUrl = Bundle.main.resourceURL?.appending(path: "Tests", directoryHint: .isDirectory).appending(path: "Triangulation", directoryHint: .isDirectory) else {
+            fatalError("Folder not found.")
+        }
+        
+        return folderUrl
+    }
+    
+    
+    func count() -> Int {
+        let fileManager = FileManager.default
+        do {
+            let contents = try fileManager.contentsOfDirectory(at: folderUrl, includingPropertiesForKeys: nil, options: [])
+            
+            // Filter the contents to get only 'json' files
+            let jsonFiles = contents.filter { $0.pathExtension == "json" }
+            
+            // Count the 'json' files
+            let jsonFilesCount = jsonFiles.count
+            
+            return jsonFilesCount
+        } catch {
+            fatalError("Could not retrieve contents of the folder: \(error)")
+        }
+    }
+    
+    func load(index: Int) -> TriangulationTest {
+        let folder = self.folderUrl
+
+        let fileName = "triangle_test_\(index).json"
+        
+        let fileURL = folder.appending(component: fileName, directoryHint: .notDirectory)
+        do {
+            let data = try Data(contentsOf: fileURL)
+
+            let decoder = JSONDecoder()
+            let decodedData = try decoder.decode(TriangulationTest.self, from: data)
+            
+            return decodedData
+        } catch {
+            fatalError("Error loading \(fileName): \(error)")
+        }
+    }
+    
+    func loadAll() -> [TriangulationTest] {
+        var result = [TriangulationTest]()
+        let count = self.count()
+        result.reserveCapacity(count)
+        for i in 0..<count {
+            result.append(self.load(index: i))
+        }
+        
+        return result
+    }
+    
+}
